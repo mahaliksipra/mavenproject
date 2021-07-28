8d23e4b9d4c61f090c74eabb9b9a2ff40020fda5
mahal <mahal@MSI>
2021-07-28 20:31:17 +0530

    first script

---
+++ maventest/src/test/java/seleniummvntest/maventest/HomePage.java
@@ -0,0 +1,9 @@
+package seleniummvntest.maventest;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+	}
+
+}
