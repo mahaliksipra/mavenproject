13b05d093e5527a6a47b78dc1a45259ffd18f82a
mahal <mahal@MSI>
2021-07-31 09:01:05 +0530

    pom.xml chabge and change in home page

--- maventest/src/test/java/seleniummvntest/maventest/HomePage.java
+++ maventest/src/test/java/seleniummvntest/maventest/HomePage.java
@@ -1,9 +1,14 @@
 package seleniummvntest.maventest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomePage {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.youtube.com/");
 	}
 
 }
